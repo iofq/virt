8416560b19a9a0bfda7e9038b39e485755b1b17d
iofq <cjriddz@protonmail.com>
2020-04-01 21:45:28 -0500

    virt-destroy now removes disk image

--- virt-destroy.sh
+++ virt-destroy.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-virsh destroy $1 && virsh undefine $1
+
+DATADIR="$PWD/disks"
+
+if [[ ! -f config ]]; then
+  echo "config file not found. using defaults..."
+else
+  source config
+fi
+
+virsh destroy $1 && virsh undefine $1 && \
+  rm $DATADIR/$1.qcow2
