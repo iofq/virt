35a469bef37eaba9e884b31c3994bad8f22f86ea
iofq <cjriddz@protonmail.com>
2020-12-07 14:21:21 -0600

    added flag for disk size, fixed a bug, cleaned up # of created files

--- virt-install.sh
+++ virt-install.sh
@@ -11,8 +11,10 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'  
 WHITE='\033[0;37m'
 
+VM_HOSTNAME=""
 VM_RAM=2048
 VM_CPU=1
+VM_DISK_SIZE="16G"
 IMAGEDIR="$PWD/images"
 DATADIR="$PWD/disks"
 VM_IMAGE="$IMAGEDIR/base.qcow2"
@@ -21,16 +23,21 @@ DEBUG=0
 VM_DELETE=0 #1 for delete, 2 for delete with data
 
 usage() {
-  echo "usage: ./virt-install.sh -h hostname -i image.qcow2" 1>&2; exit 1;
+  echo "usage: ./virt-install.sh -h hostname -i image.qcow2 -m mB(RAM) -c #CPUs -s disk size (16G format)" 1>&2
+  echo "-h hostname -d -- delete a virtual machine, keeping the virtual disk" 1>&2
+  echo "-h hostname -D -- delete a virtual machine, also deleting the virtual disk" 1>&2
+  exit 1
 }
 
 destroy() {
-  virsh destroy $VM_HOSTNAME
+[[ $VM_HOSTNAME != "" ]] && \
+  virsh destroy $VM_HOSTNAME && \
   virsh undefine $VM_HOSTNAME
 }
 
 delete() {
   rm $DATADIR/$VM_HOSTNAME.qcow2
+  rm $IMAGEDIR/.$VM_HOSTNAME-cloud-init.iso
 }
 
 if [[ ! -f config ]]; then
@@ -45,7 +52,7 @@ else
   DEBUG=""
 fi
 
-while getopts h:i:m:c:dD option
+while getopts h:i:m:c:s:dD option
 do
   case "${option}"
     in
@@ -53,6 +60,7 @@ do
     i) VM_IMAGE=${OPTARG};;
     m) VM_RAM=${OPTARG};;
     c) VM_CPU=${OPTARG};;
+    s) VM_DISK_SIZE=${OPTARG};;
     d) VM_DELETE=1;;
     D) VM_DELETE=2;;
 
@@ -66,7 +74,7 @@ VM_IMAGE=$(realpath $VM_IMAGE)
 case $VM_DELETE in
     0);;
     1) destroy && exit 0;;
-    2) destroy && delete && exit 0;;
+    2) destroy && delete; exit 0;;
     *) exit 1;;
 esac
 
@@ -82,13 +90,11 @@ while read line; do
   echo "- $line"
 done < meta-data
 
-[[ -f $IMAGEDIR/cloud-init.iso ]] && \
-    mv $IMAGEDIR/cloud-init.iso $IMAGEDIR/.cloud-init.iso.bak
 [[ ! -f user-data ]] && \
   echo -e "$RED ERROR: can't find user-data file.$RESET" && exit 1
 [[ ! -f meta-data ]] && \
   echo -e "$RED ERROR: Can't find meta-data file.$RESET" && exit 1
-genisoimage -output $IMAGEDIR/cloud-init.iso -volid cidata -J -R user-data meta-data &> .install.log && rm meta-data
+genisoimage -output $IMAGEDIR/.$VM_HOSTNAME-cloud-init.iso -volid cidata -J -R user-data meta-data &> .install.log && rm meta-data
 
 echo -e "${YELLOW}Generated cloud-init.iso"
 
@@ -110,7 +116,7 @@ if [ -f $DATADIR/$VM_HOSTNAME.qcow2 ]; then
 fi
 echo -e "$RESET"
 
-sudo qemu-img create -f qcow2 -F qcow2 -b $VM_IMAGE $DATADIR/$VM_HOSTNAME.qcow2 && \
+sudo qemu-img create -f qcow2 -F qcow2 -b $VM_IMAGE $DATADIR/$VM_HOSTNAME.qcow2 $VM_DISK_SIZE && \
   sudo chown qemu:qemu $DATADIR/$VM_HOSTNAME.qcow2 || exit 1
 
 echo -e "${YELLOW}Provisioning host...$RESET"
@@ -127,7 +133,7 @@ sudo virt-install \
   --graphics vnc \
   --noautoconsole \
   --disk=path=$DATADIR/$VM_HOSTNAME.qcow2,format=qcow2,bus=virtio \
-  --disk=path=$IMAGEDIR/cloud-init.iso,device=cdrom $DEBUG &> .install.log || exit 1
+  --disk=path=$IMAGEDIR/.$VM_HOSTNAME-cloud-init.iso,device=cdrom $DEBUG &> .install.log || exit 1
 
 	while true; do
     echo -e "$YELLOW"
